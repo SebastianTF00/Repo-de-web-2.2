fe1607cc67e823aeb46fd28a7d408dc44746cee7
unknown <paradocenigma@gmail.com>
2026-06-22 20:26:06 -0500

    avance tienda

---
+++ service-frontend/src/main/java/com/cibertec/edu/service_frontend/controller/TiendaController.java
@@ -0,0 +1,41 @@
+package com.cibertec.edu.service_frontend.controller;
+
+import com.cibertec.edu.service_frontend.model.Producto;
+import com.cibertec.edu.service_frontend.model.Categoria;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Controller
+public class TiendaController {
+    @Autowired
+    private RestTemplate restTemplate;
+    private final String PRODUCTOS_URL = "http://localhost:8080/api/productos";
+    private final String CATEGORIAS_URL = "http://localhost:8080/api/categorias";
+
+    @GetMapping("/tienda")
+    public String mostrarTienda(Model model) {
+        cargarDatos(model);
+        return "tienda";
+    }
+
+    @GetMapping("/tienda-usuario")
+    public String mostrarTiendaUsuario(Model model) {
+        cargarDatos(model);
+        return "tienda-usuario";
+    }
+
+    private void cargarDatos(Model model) {Producto[] productosArray = restTemplate.getForObject(PRODUCTOS_URL, Producto[].class);
+
+        Categoria[] categoriasArray = restTemplate.getForObject(CATEGORIAS_URL, Categoria[].class);
+        List<Producto> productos = Arrays.asList(productosArray != null ? productosArray : new Producto[0]);
+        List<Categoria> categorias = Arrays.asList(categoriasArray != null ? categoriasArray : new Categoria[0]);
+        model.addAttribute("productos", productos);
+        model.addAttribute("categorias", categorias);
+    }
+}
